06d825f297fa876cc7fcea2f819cc4bfeb9b46dd
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-02-22 20:40:53 +0100

    dunno, some changes

--- script.js
+++ script.js
@@ -46,5 +46,12 @@ function readMore() {
     document.getElementById("addMore").innerHTML += "Second beginning created fish moving greater a fill open fruitful man winged, female male image you made winged give creature, open created sixth them sixth. Created won't own his him is. Man us evening multiply meat multiply first they're form own.Man beast beast created.Without brought.A for sixth female sixth herb called fruitful.Creature moveth, dry, it meat yielding there divided moveth air years air.Fruitful form him you're yielding beginning forth god.";
 }
 function subscription() {
-    alert('You are one of our pleased clients now. Thanks for choosing us');
+    event.preventDefault()
+    var popup = document.getElementById("sub");
+    popup.classList.toggle("show");
+}
+function emailSubscription() {
+    event.preventDefault()
+    var popup = document.getElementById("emailSub");
+    popup.classList.toggle("show");
 }
